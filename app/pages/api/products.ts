245a0a67e398a312be8e75c29fd10b055451f7db
HackerBaba619 <pd4439514@gmail.com>
2024-09-20 16:47:52 +0530

    razorpay

---
+++ app/pages/api/products.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const products = [
+  { id: 1, name: 'Minimalist Shirt', price: 50, imageUrl: '/images/product1.jpg' },
+  { id: 2, name: 'Basic Trousers', price: 70, imageUrl: '/images/product2.jpg' },
+  // Add more products
+];
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    res.status(200).json(products);
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+};
