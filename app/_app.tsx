5217d29cd0c6eef0bba1381d2a95d72cf7012cb5
John <pd4439514@gmail.com>
2024-10-18 16:23:10 +0530

    Update _app.tsx

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,14 +1,14 @@
 // app/_app.tsx
 import React from 'react';
 import { AppProps } from 'next/app'; // Import AppProps from next/app
-import { CartProvider } from './components/CartContext'; // Adjust the import path as necessary
+// import { CartProvider } from './components/CartContext'; // Adjust the import path as necessary
 import '../styles/globals.css'; // Import your global styles
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <CartProvider>
+    
       <Component {...pageProps} />
-    </CartProvider>
+   
   );
 };
 
